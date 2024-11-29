abf42787cbb12286c2dc9ae8563283a8e3053ce7
Suphachok Yeamsri <suphachok@kkumail.com>
2024-11-29 15:43:06 +0700

    create HTML template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,13 +2,34 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
+	"text/template"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
-	w.Write([]byte("Hello from Snippetbox"))
+	
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/pages/home.tmpl",
+		"./ui/html/partials/nav.tmpl",
+	}
+
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "base",nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error" , http.StatusInternalServerError)
+	}
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
