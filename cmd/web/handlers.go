db217699a5f48b1e11c51a9a200ba8632d67a340
Suphachok Yeamsri <suphachok@kkumail.com>
2025-01-05 12:11:09 +0700

    create function to queries multiple SQL

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,69 +1,74 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
-	"strconv"
-	"text/template"
-
-	"snippetbox.suphachok.net/internal/models"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
 
-	files := []string{
-		"./ui/html/base.tmpl",
-		"./ui/html/pages/home.tmpl",
-		"./ui/html/partials/nav.tmpl",
-	}
-
-	ts, err := template.ParseFiles(files...)
+	snippets, err := app.snippets.Latest()
 	if err != nil {
-		app.serverError(w, r, err) //Use the serverError() helper
+		app.serverError(w, r, err)
 		return
 	}
 
-	err = ts.ExecuteTemplate(w, "base", nil)
-	if err != nil {
-		app.serverError(w, r, err)  // Use the serverError() helper.
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%+v\n", snippet)
 	}
-}
+	/*
+			files := []string{
+				"./ui/html/base.tmpl",
+				"./ui/html/pages/home.tmpl",
+				"./ui/html/partials/nav.tmpl",
+			}
 
-func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.PathValue("id"))
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
+			ts, err := template.ParseFiles(files...)
+			if err != nil {
+				app.serverError(w, r, err) //Use the serverError() helper
+				return
+			}
 
-	snippet, err := app.snippets.Get(id)
-	if err != nil {
-		if errors.Is(err, models.ErrNoRecord) {
-			http.NotFound(w, r)
-		} else {
-			app.serverError(w, r, err)
+			err = ts.ExecuteTemplate(w, "base", nil)
+			if err != nil {
+				app.serverError(w, r, err)  // Use the serverError() helper.
+			}
 		}
-		return
-	}
 
-	fmt.Fprintf(w, "%+v", snippet)
-}
+		func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
+			id, err := strconv.Atoi(r.PathValue("id"))
+			if err != nil || id < 1 {
+				http.NotFound(w, r)
+				return
+			}
 
-func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a form for creating a new snippet..."))
-}
+			snippet, err := app.snippets.Get(id)
+			if err != nil {
+				if errors.Is(err, models.ErrNoRecord) {
+					http.NotFound(w, r)
+				} else {
+					app.serverError(w, r, err)
+				}
+				return
+			}
 
-func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	title := "0 snail"
-	content := "0 snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
-	expires := 7
+			fmt.Fprintf(w, "%+v", snippet)
+		}
 
-	id, err := app.snippets.Insert(title, content, expires)
-	if err != nil {
-		app.serverError(w,r,err)
-		return
-	}
-	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
+		func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("Display a form for creating a new snippet..."))
+		}
+
+		func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+			title := "0 snail"
+			content := "0 snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
+			expires := 7
+
+			id, err := app.snippets.Insert(title, content, expires)
+			if err != nil {
+				app.serverError(w,r,err)
+				return
+			}
+			http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther) */
 }
