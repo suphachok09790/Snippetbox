78365aa9c238eb74d348e18036f75df9042e4dda
Suphachok Yeamsri <suphachok@kkumail.com>
2025-01-08 18:29:25 +0700

    create page show all snippet

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -19,81 +19,79 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	for _, snippet := range snippets {
-		fmt.Fprintf(w, "%+v\n", snippet)
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/pages/home.tmpl",
+		"./ui/html/partials/nav.tmpl",
 	}
-	
-			files := []string{
-				"./ui/html/base.tmpl",
-				"./ui/html/pages/home.tmpl",
-				"./ui/html/partials/nav.tmpl",
-			}
 
-			ts, err := template.ParseFiles(files...)
-			if err != nil {
-				app.serverError(w, r, err) //Use the serverError() helper
-				return
-			}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, r, err) //Use the serverError() helper
+		return
+	}
 
-			err = ts.ExecuteTemplate(w, "base", nil)
-			if err != nil {
-				app.serverError(w, r, err)  // Use the serverError() helper.
-			}
-		}
+	data := templateData{
+		Snippets: snippets,
+	}
 
-		func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-			id, err := strconv.Atoi(r.PathValue("id"))
-			if err != nil || id < 1 {
-				http.NotFound(w, r)
-				return
-			}
-
-			snippet, err := app.snippets.Get(id)
-			if err != nil {
-				if errors.Is(err, models.ErrNoRecord) {
-					http.NotFound(w, r)
-				} else {
-					app.serverError(w, r, err)
-				}
-				return
-			}
-			files := []string{
-				"./ui/html/base.tmpl",
-				"./ui/html/partials/nav.tmpl",
-				"./ui/html/pages/view.tmpl",
-			}
-
-			ts, err := template.ParseFiles(files...)
-			if err != nil {
-				app.serverError(w, r, err)
-				return
-			}
-
-			data := templateData{
-				Snippet: snippet,
-			}
-
-			err = ts.ExecuteTemplate(w, "base", data)
-			if err != nil {
-				app.serverError(w, r, err)
-			}
-		}
-
-		func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-			w.Write([]byte("Display a form for creating a new snippet..."))
-		}
-
-		func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-			title := "0 snail"
-			content := "0 snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
-			expires := 7
-
-			id, err := app.snippets.Insert(title, content, expires)
-			if err != nil {
-				app.serverError(w,r,err)
-				return
-			}
-			http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther) 
+	err = ts.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		app.serverError(w, r, err) // Use the serverError() helper.
+	}
 }
 
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
 
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			http.NotFound(w, r)
+		} else {
+			app.serverError(w, r, err)
+		}
+		return
+	}
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/view.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	data := templateData{
+		Snippet: snippet,
+	}
+
+	err = ts.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		app.serverError(w, r, err)
+	}
+}
+
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet..."))
+}
+
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	title := "0 snail"
+	content := "0 snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
+	expires := 7
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
+}
