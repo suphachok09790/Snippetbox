92cf917b31420958305fdcf0062655b018ef197f
Suphachok Yeamsri <suphachok@kkumail.com>
2025-01-08 23:56:07 +0700

    custom template function for show time

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,49 +1,62 @@
 package main
 
 import (
-	"path/filepath"
 	"html/template"
+	"path/filepath"
+	"time"
+
 	"snippetbox.suphachok.net/internal/models"
 )
 
 type templateData struct {
-	Snippet models.Snippet
-	Snippets []models.Snippet
+	CurrentYear int
+	Snippet     models.Snippet
+	Snippets    []models.Snippet
+}
+
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
 	// Initialize a new map to act as the cache.
 	cache := map[string]*template.Template{}
-	
+
 	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
 	if err != nil {
-	return nil, err
+		return nil, err
 	}
 	// Loop through the page filepaths one-by-one.
 	for _, page := range pages {
-	// Extract the file name (like 'home.tmpl') from the full filepath
-	// and assign it to the name variable.
-	name := filepath.Base(page)
+		// Extract the file name (like 'home.tmpl') from the full filepath
+		// and assign it to the name variable.
+		name := filepath.Base(page)
 
-	ts, err := template.ParseFiles("./ui/html/base.tmpl")
-	if err != nil {
-		return nil, err
-	}
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
+		if err != nil {
+			return nil, err
+		}
 
-	ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
-	if err != nil {
-		return nil, err
-	}
+		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
+		if err != nil {
+			return nil, err
+		}
 
-	ts, err = ts.ParseFiles(page)
-	if err != nil {
-		return nil, err
-	}
+		ts, err = ts.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
 
-	cache[name] =ts
+		cache[name] = ts
 	}
 	// Return the map.
 	return cache, nil
 }
-	
+
+
+
 
