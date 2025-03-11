c2fdb482d46a490f0e3bada87c3ad6cdd5da8004
Suphachok Yeamsri <suphachok@kkumail.com>
2025-03-11 16:33:01 +0700

    Create function log out and authorization

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -53,6 +53,7 @@ func (app *application) newTemplateData(r *http.Request) templateData {
 	return templateData{
 		CurrentYear: time.Now().Year(),
 		Flash: app.sessionManager.PopString(r.Context(),"flash"),
+		IsAuthenticated: app.isAuthenticated(r),
 	}
 }
 
@@ -73,4 +74,8 @@ func (app *application) decodePostForm(r *http.Request, dst any) error {
 		return err
 	}
 	return nil
+}
+
+func (app *application) isAuthenticated(r *http.Request) bool {
+	return app.sessionManager.Exists(r.Context(), "authenticatedUserID")
 }
